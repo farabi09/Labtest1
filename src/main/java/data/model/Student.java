084187c9518f4de95a6e5f8d1fece23ede758bda
Triple_F <jimmy28riordon@gmail.com>
2018-04-25 07:00:00 +0600

    2nd commit

--- src/main/java/data/model/Student.java
+++ src/main/java/data/model/Student.java
@@ -3,6 +3,15 @@ package data.model;
 public class Student {
     private Integer studentId;
     private String studentName;
+    private String studentAddress;
+
+    public String getStudentAddress() {
+        return studentAddress;
+    }
+
+    public void setStudentAddress(String studentAddress) {
+        this.studentAddress = studentAddress;
+    }
 
     public Student() {
     }
